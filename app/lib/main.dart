bee1124a3f349982960098404a2d5efba600a85e
chihabMe <chihab.mg.me@gmail.com>
2024-02-02 14:50:41 +0100

    created a flutter app

---
+++ app/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext) {
+    return MaterialApp(
+      title: "myapp",
+      home: Text("hi"),
+    );
+  }
+}
