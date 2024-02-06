834cf0eeab47534576f7ab1c45db76873b9066ed
chihabMe <chihab.mg.me@gmail.com>
2024-02-06 21:01:50 +0100

    added a search bar

--- app/lib/screens/home/home.screen.dart
+++ app/lib/screens/home/home.screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:app/widgets/BottomNavigator.dart';
 import 'package:app/widgets/categoriesList/categories.filters.list.dart';
 import 'package:app/widgets/categoriesList/categories.list.dart';
@@ -5,53 +6,68 @@ import 'package:app/widgets/placesList/places.list.dart';
 import 'package:app/widgets/regionsList/regions.list.dart';
 import 'package:app/widgets/search/search.dart';
 import 'package:app/widgets/ui/home.title.dart';
-import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-        child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 40,
-              ),
-              // ignore: prefer_const_constructors
-              HomeTitle(
-                text: "Regions",
-                size: 24,
-                fontWeight: FontWeight.w600,
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              RegionsList(),
-              SizedBox(
-                height: 20,
-              ),
-              PlacesList(),
-              SizedBox(
-                height: 30,
-              ),
-              HomeTitle(
-                text: "Categories",
-                size: 24,
-                fontWeight: FontWeight.w600,
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              CategoriesFiltersList(),
-              SizedBox(
-                height: 15,
-              ),
-              CategoiresList()
-            ],
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 40),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  height: 57,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 248, 243, 243),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Row(
+                    children: [
+                      IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.search,
+                            size: 20,
+                            color: Color.fromARGB(255, 166, 166, 166),
+                          )),
+                      Expanded(
+                          child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Explore  new places",
+                            hintStyle: TextStyle(fontSize: 14)),
+                      )),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                HomeTitle(
+                  text: "Regions",
+                  size: 24,
+                  fontWeight: FontWeight.w600,
+                ),
+                SizedBox(height: 15),
+                RegionsList(),
+                SizedBox(height: 20),
+                PlacesList(),
+                SizedBox(height: 30),
+                HomeTitle(
+                  text: "Categories",
+                  size: 24,
+                  fontWeight: FontWeight.w600,
+                ),
+                SizedBox(height: 15),
+                CategoriesFiltersList(),
+                SizedBox(height: 15),
+                CategoiresList(),
+              ],
+            ),
           ),
         ),
       ),
