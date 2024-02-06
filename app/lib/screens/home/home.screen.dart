215c952666e43e70de675064bcd6435cfd92f649
victorlicht <mihoubiabderrahmane09@gmail.com>
2024-02-06 23:16:05 +0100

    Add Searching Functionality Working VeryFine

--- app/lib/screens/home/home.screen.dart
+++ app/lib/screens/home/home.screen.dart
@@ -2,6 +2,7 @@ import 'package:app/widgets/BottomNavigator.dart';
 import 'package:app/widgets/categoriesList/categories.list.dart';
 import 'package:app/widgets/placesList/places.list.dart';
 import 'package:app/widgets/regionsList/regions.list.dart';
+import 'package:app/widgets/search_filter/search.dart';
 import 'package:app/widgets/ui/home.title.dart';
 import 'package:flutter/material.dart';
 
@@ -48,7 +49,18 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
+
       bottomNavigationBar: BottomNavigator(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SearchPlace()),
+          );
+        },
+        backgroundColor: Colors.blue,
+        child: Icon(Icons.search, color: Colors.white,),
+      ),
     );
   }
 }
--- app/lib/widgets/Spacer.dart
+++ app/lib/widgets/Spacer.dart
@@ -2,5 +2,7 @@ import 'package:flutter/material.dart';
 
 class Spacer extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
 }
