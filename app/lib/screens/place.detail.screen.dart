4e7253e1235ac7ea5d2a33c809b251bb63278cf2
chihabMe <chihab.mg.me@gmail.com>
2024-02-07 15:29:52 +0100

    added a screen detail

---
+++ app/lib/screens/place.detail.screen.dart
@@ -0,0 +1,147 @@
+import 'package:app/interfaces/i.place.dart';
+import 'package:flutter/material.dart';
+
+class PlaceDetailScreen extends StatefulWidget {
+  final IPlace place;
+
+  PlaceDetailScreen({required this.place});
+
+  @override
+  _PlaceDetailScreenState createState() => _PlaceDetailScreenState();
+}
+
+class _PlaceDetailScreenState extends State<PlaceDetailScreen> {
+  String _selectedImage = '';
+
+  @override
+  void initState() {
+    super.initState();
+    // Set the initial selected image to the main image
+    _selectedImage = widget.place.image;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Stack(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Image.asset(
+                      _selectedImage,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    top: 16,
+                    left: 16,
+                    child: IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 8),
+            SizedBox(
+              height: 100,
+              child: ListView.builder(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                scrollDirection: Axis.horizontal,
+                itemCount: widget.place.images.length,
+                itemBuilder: (context, index) {
+                  String image = widget.place.images[index];
+                  return GestureDetector(
+                    onTap: () {
+                      // Update the main image when an image in the list is tapped
+                      setState(() {
+                        _selectedImage = image;
+                      });
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.only(right: 8),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: AspectRatio(
+                          aspectRatio: 16 /
+                              9, // You can adjust the aspect ratio as needed
+                          child: Image.asset(
+                            image,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.place.name,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Row(
+                    children: [
+                      Icon(Icons.star, color: Colors.amber, size: 20),
+                      SizedBox(width: 4),
+                      Text(
+                        '${widget.place.rating} (${widget.place.numberOfRates} Reviews)',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'State: ${widget.place.state}',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    'Description:',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    widget.place.description,
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 8),
+          ],
+        ),
+      ),
+    );
+  }
+}
